d323c2be512f2800cd007354b6e2ffc9d5dad77c
Brenokly <breno.moura@alunos.ufersa.edu.br>
2023-09-21 18:20:10 -0300

    controller

--- br/edu/ufersa/poo/Pizzaria/controller/TelaLogin.java
+++ br/edu/ufersa/poo/Pizzaria/controller/TelaLogin.java
@@ -1,6 +1,15 @@
 package br.edu.ufersa.poo.Pizzaria.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class TelaLogin {
+
+  @FXML private TextField Login;
+  @FXML private PasswordField Senha;
+  @FXML private Button Entrar;
   public void Autenticar(){
   
   }
